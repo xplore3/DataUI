d9cb43d156a53540dc72268ffbf7b6b5bb111297
Xplore3 <Vincent.Rapheal.07@gmail.com>
2025-06-28 22:58:22 +0800

    Bug fix

--- src/views/chat/index.tsx
+++ src/views/chat/index.tsx
@@ -383,6 +383,7 @@ const Chat = () => {
           ]);
         })
         .finally(async () => {
+          setText('');
           //setLoading(false);
           await handlerStatus();
         });
