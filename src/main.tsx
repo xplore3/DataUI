53bd54540a6ecfd47dc7e3c15dc6127efc6f25b2
Auden <49225795+zhongchunfengyyds@users.noreply.github.com>
2025-07-06 16:17:30 +0800

    白屏修复

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+// Add polyfill for Object.hasOwn to support older environments
+if (!Object.hasOwn) {
+  Object.hasOwn = function(obj, prop) {
+    return Object.prototype.hasOwnProperty.call(obj, prop);
+  };
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
