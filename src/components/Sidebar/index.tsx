c8de82b14cc1a501fb8a4b4fe90ca6201d37ab47
Xplore3 <Vincent.Rapheal.07@gmail.com>
2025-07-23 19:31:23 +0800

    Update the side bar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './index.less';
 
 const Sidebar: React.FC = () => {
+
+  const navigate = useNavigate();
+
+  const onHome = () => {
+    navigate('/ip');
+  };
+
+  const onUser = () => {
+    navigate('/user');
+  };
+
+  const onMore = () => {
+    //navigate('/ip');
+  };
+
   return (
     <aside className="sidebar">
-      <div className="nav-item">首页</div>
-      <div className="nav-item">我的</div>
-      <div className="nav-item">更多</div>
+      <div className="nav-item" onClick={onHome}>首页</div>
+      <div className="nav-item" onClick={onUser}>我的</div>
+      <div className="nav-item" onClick={onMore}>更多</div>
     </aside>
   );
 };
--- src/views/chat/index.tsx
+++ src/views/chat/index.tsx
@@ -95,6 +95,8 @@ const Chat = () => {
       localStorage.removeItem('welcomeMessage');
     }
 
+    handleKeyPress('IP定位');
+
     setTips('请输入你的数据处理指令');
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
