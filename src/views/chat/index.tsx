f4cff1501e992c5496cb2a25d75ac859a487dee1
Xplore3 <Vincent.Rapheal.07@gmail.com>
2025-09-05 04:10:45 +0800

    Update the routine buttons

--- src/views/chat/index.tsx
+++ src/views/chat/index.tsx
@@ -136,7 +136,7 @@ const Chat = () => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const isTranslatingRef = useRef(false);
   const chatContainerRef = useRef<HTMLDivElement | null>(null);
-  const keyList = ['模板', '热门话题', '热门视频', '热门新闻', '热门商品', 'HotToken'];
+  const keyList = ['模板', '热门话题', '热门视频', '热门新闻', '热门商品'];
 
   // Load saved messages from local storage and initialize displayText
   useEffect(() => {
